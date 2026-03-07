a471b0fcdcb9f71caaadc4ccc6ae15111d059221
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-07 20:15:01 +0100

    chore(app): adds paths to shadcn-ui components in vite config & tsconfig

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -10,5 +11,10 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@shadcn-ui": path.resolve(__dirname, "./src/shadcn-ui"),
+    },
   },
 });
