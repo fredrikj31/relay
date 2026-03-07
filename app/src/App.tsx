54ba7bbf160bae8b073c4ee58f4f9e313fcc3ad0
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-07 20:16:29 +0100

    chore(app): installs shadcn-ui button component & adds to App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@shadcn-ui/components/ui/button";
+
 export const App = () => {
-  return <h1 className="text-xl">Hello World</h1>;
+  return (
+    <>
+      <h1 className="text-xl">Hello World</h1>
+      <Button onClick={() => console.log("Hello World")}>Click Me</Button>
+    </>
+  );
 };
