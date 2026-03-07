6bce3dde3b1b4017f3a77db5d11cd69da1e4a567
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-07 20:37:39 +0100

    fix(app): fixes linting issue with vite.config.ts __dirname variable

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export default defineConfig({
   envDir: "../",
   plugins: [react(), tailwindcss()],
