c4ffccdddc9473a9361a3c284368102652b1ddfb
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-25 20:28:27 +0100

    feat(api): adds database service client with database connection

---
+++ api/src/services/database/client.ts
@@ -0,0 +1,104 @@
+import { FastifyInstance } from "fastify";
+import { fastifyPlugin } from "fastify-plugin";
+import {
+  CommonQueryMethods,
+  createBigintTypeParser,
+  createIntervalTypeParser,
+  createNumericTypeParser,
+  createPool,
+  Interceptor,
+  QueryResultRow,
+  SchemaValidationError,
+} from "slonik";
+import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation";
+import z from "zod";
+import { logger } from "../../logger";
+
+interface DatabasePluginOptions {
+  dbHost: string;
+  dbPort: string;
+  dbUser: string;
+  dbPassword: string;
+  dbName: string;
+}
+
+const database = async (
+  fastify: FastifyInstance,
+  opts: DatabasePluginOptions,
+) => {
+  try {
+    const pool = await createPool(
+      `postgresql://${opts.dbUser}:${opts.dbPassword}@${opts.dbHost}:${opts.dbPort}/${opts.dbName}`,
+      {
+        interceptors: [
+          createFieldNameTransformationInterceptor({
+            test: (field) => {
+              return (
+                field.name !== "__typename" && /^[\d_a-z]+$/u.test(field.name)
+              );
+            },
+          }),
+          zodParserInterceptor(),
+        ],
+        typeParsers: [
+          createBigintTypeParser(),
+          createIntervalTypeParser(),
+          createNumericTypeParser(),
+          customTimestampParser,
+        ],
+      },
+    );
+    fastify.decorate("database", pool);
+  } catch (error: unknown) {
+    logger.fatal(error, "Unable to connect to database");
+    throw new Error("Unable to connect to database!", {
+      cause: error,
+    });
+  }
+};
+export const databasePlugin = fastifyPlugin(database);
+
+declare module "fastify" {
+  export interface FastifyInstance {
+    database: CommonQueryMethods;
+  }
+}
+
+const customTimestampParser = {
+  name: "timestamp",
+  parse: (s: string): string => {
+    return new Date(Date.parse(s + " UTC")).toISOString();
+  },
+};
+
+const zodParserInterceptor = (): Interceptor => {
+  return {
+    name: "zod-result-parser-interceptor",
+    // If you are not going to transform results using Zod, then you should use `afterQueryExecution` instead.
+    // Future versions of Zod will provide a more efficient parser when parsing without transformations.
+    // You can even combine the two – use `afterQueryExecution` to validate results, and (conditionally)
+    // transform results as needed in `transformRow`.
+    transformRowAsync: async (queryContext, query, result) => {
+      const { resultParser } = queryContext;
+
+      if (!resultParser) {
+        return result;
+      }
+
+      // It is recommended (but not required) to parse async to avoid blocking the event loop during validation
+      const validationResult = await (
+        resultParser as z.ZodSchema
+      ).safeParseAsync(result);
+
+      if (!validationResult.success) {
+        throw new SchemaValidationError(
+          query,
+          result,
+          validationResult.error.issues,
+        );
+      }
+
+      return validationResult.data as QueryResultRow;
+    },
+  };
+};
