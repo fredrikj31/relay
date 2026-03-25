3939ea7d0221094d4e9a91853796a7869c891143
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-25 20:55:32 +0100

    feat(api): adds database query to check if username exist & create profile

---
+++ api/src/services/database/queries/profile/createProfile.ts
@@ -0,0 +1,61 @@
+import { CommonQueryMethods, sql } from "slonik";
+import { Profile, ProfileSchema } from "../../../../types/profile";
+import { InternalServerError } from "../../../../errors/server";
+import { logger } from "../../../../logger";
+
+interface CreateProfileOptions {
+  userId: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  gender: string;
+}
+
+export const createProfile = async (
+  database: CommonQueryMethods,
+  {
+    userId,
+    username,
+    firstName,
+    lastName,
+    birthDate,
+    gender,
+  }: CreateProfileOptions,
+): Promise<Profile> => {
+  try {
+    return await database.one(sql.type(ProfileSchema)`
+      INSERT INTO
+        profile (
+          user_id,
+          username,
+          first_name,
+          last_name,
+          birth_date,
+          gender,
+          created_at,
+          updated_at,
+          deleted_at
+        )
+      VALUES
+        (
+          ${userId},
+          ${username},
+          ${firstName},
+          ${lastName},
+          ${birthDate},
+          ${gender},
+          ${new Date().toISOString()},
+          NULL,
+          NULL
+        )
+      RETURNING *;
+    `);
+  } catch (error) {
+    logger.error({ error }, "Error while creating profile in database.");
+    throw new InternalServerError({
+      code: "unknown-error-creating-profile",
+      message: "Unknown error when trying to create profile",
+    });
+  }
+};
---
+++ api/src/services/database/queries/profile/doesUsernameExist.ts
@@ -0,0 +1,37 @@
+import { CommonQueryMethods, sql } from "slonik";
+import { ProfileSchema } from "../../../../types/profile";
+import { logger } from "../../../../logger";
+import { InternalServerError } from "../../../../errors/server";
+
+interface DoesUsernameExistOptions {
+  username: string;
+}
+
+export const doesUsernameExist = async (
+  database: CommonQueryMethods,
+  { username }: DoesUsernameExistOptions,
+): Promise<boolean> => {
+  const user = await database
+    .any(
+      sql.type(ProfileSchema)`
+      SELECT
+        *
+      FROM
+        profile
+      WHERE
+        username = ${username};
+    `,
+    )
+    .catch((error) => {
+      logger.error({ error }, "Error while checking if username exists");
+      throw new InternalServerError({
+        code: "failed-checking-username-exists",
+        message: "Unknown error occurred when checking if username exists",
+      });
+    });
+
+  if (user.length === 0) {
+    return false;
+  }
+  return true;
+};
