cd3f00297bbdd95c385a69a4fdb6f569273505c3
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-26 20:54:59 +0100

    feat(api): adds route action to login user

--- api/src/routes/actions/index.ts
+++ api/src/routes/actions/index.ts
@@ -4,6 +4,7 @@ import { ProfileSchema } from "../../types/profile";
 import { signupHandler } from "./handlers/signup";
 import z from "zod";
 import { AccountSchema } from "../../types/account";
+import { loginHandler } from "./handlers/login";
 
 export const actionRoutes: FastifyPluginAsync = async (instance) => {
   const app = instance.withTypeProvider<ZodTypeProvider>();
@@ -41,6 +42,39 @@ export const actionRoutes: FastifyPluginAsync = async (instance) => {
     async (req, res) => {
       const createdUser = await signupHandler({ database, user: req.body });
       return res.send(createdUser);
+    },
+  );
+
+  app.post(
+    "/login",
+    {
+      schema: {
+        summary: "Login a user",
+        description: "Logs in the user and returns access & refresh tokens",
+        tags: ["actions"],
+        body: z.object({
+          email: z.string(),
+          password: z.string(),
+        }),
+        response: {
+          "204": z.void(),
+        },
+      },
+    },
+    async (req, res) => {
+      const { accessToken, refreshToken } = await loginHandler({
+        database,
+        credentials: req.body,
+      });
+
+      // Sets access and refresh token in the cookies
+      res.setCookie("access_token", accessToken.token, {
+        expires: new Date(accessToken.expiresAt),
+      });
+      res.setCookie("refresh_token", refreshToken.token, {
+        expires: new Date(refreshToken.expiresAt),
+      });
+      return res.status(204).send();
     },
   );
 };
