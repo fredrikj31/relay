c283533d1a72ad9b96c8f75bfb062542fa054590
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-27 21:42:49 +0100

    feat(app): adds design for login form

--- app/src/routes/login/route.tsx
+++ app/src/routes/login/route.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Send } from "lucide-react";
+import { LoginForm } from "./components/LoginForm";
 
 export const Route = createFileRoute("/login")({
   component: Login,
 });
 
 function Login() {
-  return <div className="p-2">Hello from Login!</div>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex size-8 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Send className="size-4" />
+          </div>
+          Relay
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
