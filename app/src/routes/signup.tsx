0b24e9b899ccc44ad165792b4a6be6e64217952e
Fredrik Johansen <fredrik1206@gmail.com>
2026-03-27 20:46:32 +0100

    feat(app): adds tanstack router with file based routing & sample pages

---
+++ app/src/routes/signup.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/signup")({
+  component: Signup,
+});
+
+function Signup() {
+  return <div className="p-2">Hello from Signup!</div>;
+}
