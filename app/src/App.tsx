ae5c8a1313c3193db1e9926ecca663a75953d920
Fredrik Johansen <fredrik1206@gmail.com>
2026-04-11 14:43:30 +0200

    refactor(app): moves login route from tanstack router to react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { NavbarProvider } from "./providers/navbar";
 import { SignupRoute } from "./routes/signup/route";
+import { LoginRoute } from "./routes/login/route";
+
 export const App = () => {
   return (
     <BrowserRouter>
       <NavbarProvider>
         <Routes>
           <Route path="/signup" element={<SignupRoute />} />
+          <Route path="/login" element={<LoginRoute />} />
         </Routes>
       </NavbarProvider>
     </BrowserRouter>
