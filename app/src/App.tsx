92594db0052844d4476a7b460ccc25e4989426b4
Fredrik Johansen <fredrik1206@gmail.com>
2026-04-11 14:52:35 +0200

    refactor(app): moves index route from tanstack router to react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,6 +6,7 @@ import { AuthenticatedRouteLayout } from "./routes/_authenticated/route";
 import { ContactsRoute } from "./routes/_authenticated.contacts/route";
 import { ArchiveRoute } from "./routes/_authenticated.archive/route";
 import { SettingsRoute } from "./routes/_authenticated.settings/route";
+import { IndexRoute } from "./routes/_authenticated.index/route";
 
 export const App = () => {
   return (
@@ -15,6 +16,7 @@ export const App = () => {
           <Route path="/signup" element={<SignupRoute />} />
           <Route path="/login" element={<LoginRoute />} />
           <Route path="/" element={<AuthenticatedRouteLayout />}>
+            <Route index element={<IndexRoute />} />
             <Route path="/contacts" element={<ContactsRoute />} />
             <Route path="/archive" element={<ArchiveRoute />} />
             <Route path="/settings" element={<SettingsRoute />} />
--- app/src/routes/_authenticated.index/route.tsx
+++ app/src/routes/_authenticated.index/route.tsx
@@ -1,9 +1,3 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/")({
-  component: Index,
-});
-
-function Index() {
+export const IndexRoute = () => {
   return <h1>Index Page</h1>;
-}
+};
