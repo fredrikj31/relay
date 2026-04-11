b9b906e4e83030368b58e8046a4cfc7e2881168e
Fredrik Johansen <fredrik1206@gmail.com>
2026-04-11 14:51:29 +0200

    refactor(app): moves settings route from tanstack router to react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,7 @@ import { LoginRoute } from "./routes/login/route";
 import { AuthenticatedRouteLayout } from "./routes/_authenticated/route";
 import { ContactsRoute } from "./routes/_authenticated.contacts/route";
 import { ArchiveRoute } from "./routes/_authenticated.archive/route";
+import { SettingsRoute } from "./routes/_authenticated.settings/route";
 
 export const App = () => {
   return (
@@ -16,6 +17,7 @@ export const App = () => {
           <Route path="/" element={<AuthenticatedRouteLayout />}>
             <Route path="/contacts" element={<ContactsRoute />} />
             <Route path="/archive" element={<ArchiveRoute />} />
+            <Route path="/settings" element={<SettingsRoute />} />
           </Route>
         </Routes>
       </NavbarProvider>
