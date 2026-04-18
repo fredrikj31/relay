eca6992b98d2edd8fc9885863fe3a9e002305fc4
Fredrik Johansen <fredrik1206@gmail.com>
2026-04-18 21:04:49 +0200

    feat(api): adds Contact schema and type

--- api/src/types/contact.ts
+++ api/src/types/contact.ts
@@ -17,3 +17,13 @@ export const ContactRequestSchema = z.object({
   status: ContactRequestStatusSchema,
 });
 export type ContactRequest = z.infer<typeof ContactRequestSchema>;
+
+export const ContactSchema = z.object({
+  id: z.uuid(),
+  createdAt: z.iso.datetime(),
+  updatedAt: z.iso.datetime().nullable(),
+  deletedAt: z.iso.datetime().nullable(),
+  accountId: z.uuid(),
+  contactId: z.uuid(),
+});
+export type Contact = z.infer<typeof ContactSchema>;
