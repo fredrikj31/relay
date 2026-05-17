983b79b2ec3eb4eda08e3d2182f0b6b929278b56
Fredrik Johansen <fredrik1206@gmail.com>
2026-05-17 10:32:13 +0200

    refactor(app): moves query keys list sent/received contact requests
    Moves query keys for listing sent & received contact requests into query keys file

--- app/src/api/contacts/listReceivedContactRequests/useListReceivedContactRequests.ts
+++ app/src/api/contacts/listReceivedContactRequests/useListReceivedContactRequests.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { listReceivedContactRequests } from "./listReceivedContactRequests";
+import { queryKeys } from "../../queryKeys";
 
 export const useListReceivedContactRequests = () => {
   return useQuery({
-    queryKey: ["contacts", "requests", "received"],
+    queryKey: queryKeys.contacts.requests.received,
     queryFn: () => listReceivedContactRequests(),
     retry: 3,
     staleTime: 300_000, // 5 minutes
