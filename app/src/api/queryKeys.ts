b196c94201e2398210f8429524e96439bd03f0b6
Fredrik Johansen <fredrik1206@gmail.com>
2026-05-17 11:06:44 +0200

    refactor(app): moves list contacts query key to queryKeys.ts file

--- app/src/api/queryKeys.ts
+++ app/src/api/queryKeys.ts
@@ -1,6 +1,7 @@
 export const queryKeys = {
   currentAccount: ["accounts", "me"] as const,
   contacts: {
+    list: ["contacts", "list"] as const,
     requests: {
       received: ["contacts", "requests", "received"] as const,
       sent: ["contacts", "requests", "sent"] as const,
