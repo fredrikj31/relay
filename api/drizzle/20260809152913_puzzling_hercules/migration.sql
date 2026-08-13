2de7cc6ad91bf6117adfe625e6b44ac2a96adc92
Fredrik Johansen <fredrik1206@gmail.com>
2026-08-13 19:15:47 +0200

    feat(api): adds database migration sql file for contact request table

---
+++ api/drizzle/20260809152913_puzzling_hercules/migration.sql
@@ -0,0 +1,15 @@
+CREATE TYPE "contact_request_status" AS ENUM('PENDING', 'ACCEPTED', 'DECLINED');--> statement-breakpoint
+CREATE TABLE "contact_request" (
+	"id" text PRIMARY KEY,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp,
+	"deleted_at" timestamp,
+	"sender_user_id" text NOT NULL,
+	"receiver_user_id" text NOT NULL,
+	"status" "contact_request_status" NOT NULL
+);
+--> statement-breakpoint
+CREATE INDEX "contact_request_sender_user_id_index" ON "contact_request" ("sender_user_id");--> statement-breakpoint
+CREATE INDEX "contact_request_receiver_user_id_index" ON "contact_request" ("receiver_user_id");--> statement-breakpoint
+ALTER TABLE "contact_request" ADD CONSTRAINT "contact_request_sender_user_id_user_id_fkey" FOREIGN KEY ("sender_user_id") REFERENCES "user"("id");--> statement-breakpoint
+ALTER TABLE "contact_request" ADD CONSTRAINT "contact_request_receiver_user_id_user_id_fkey" FOREIGN KEY ("receiver_user_id") REFERENCES "user"("id");
