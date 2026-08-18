485ba03c3c0f9d1c8dfdeabee3241d610bebb7da
Fredrik Johansen <fredrik1206@gmail.com>
2026-08-18 18:55:46 +0200

    feat(api): refactors delete contact request to drizzle orm

---
+++ api/src/services/drizzle-database/queries/contact/deleteContactRequest.ts
@@ -0,0 +1,40 @@
+import { databaseClient } from "../../client";
+import { ContactRequest, contactRequest } from "../../schemas/contact";
+import { logger } from "../../../../logger";
+import { InternalServerError } from "../../../../errors/server";
+import { and, eq } from "drizzle-orm";
+
+export const deleteContactRequest = async ({
+  id,
+  senderUserId,
+}: Pick<ContactRequest, "id" | "senderUserId">): Promise<ContactRequest> => {
+  const result = await databaseClient
+    .update(contactRequest)
+    .set({ deletedAt: new Date() })
+    .where(
+      and(
+        eq(contactRequest.id, id),
+        eq(contactRequest.senderUserId, senderUserId),
+        eq(contactRequest.status, "PENDING"),
+      ),
+    )
+    .returning()
+    .catch((error) => {
+      logger.error(error, "Error while deleting contact request.");
+      throw new InternalServerError({
+        code: "unknown-error-deleting-contact-request",
+        message: "Unknown error when trying to delete contact request",
+      });
+    });
+
+  const row = result[0];
+  if (!row) {
+    throw new InternalServerError({
+      code: "error-deleting-contact-request-no-rows",
+      message:
+        "Error deleting contact request. Database query did not return any rows.",
+    });
+  }
+
+  return row;
+};
