b54e0dd091e264de999137e67a41ed2be76d1e9e
Fredrik Johansen <fredrik1206@gmail.com>
2026-08-21 19:04:15 +0200

    feat(api): adds openAPI plugin to better auth client

--- api/src/services/auth/client.ts
+++ api/src/services/auth/client.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { databaseClient } from "../drizzle-database/client";
 import * as schema from "../drizzle-database/schemas/auth";
-import { username } from "better-auth/plugins";
+import { username, openAPI } from "better-auth/plugins";
 import { config } from "../../config";
 
 export const authClient = betterAuth({
@@ -20,5 +20,10 @@ export const authClient = betterAuth({
     autoSignIn: false,
     requireEmailVerification: false,
   },
-  plugins: [username()],
+  plugins: [
+    username(),
+    openAPI({
+      disableDefaultReference: true,
+    }),
+  ],
 });
